a8952408ae444cc2f9daea64878b2c33e61b0030
Ramir Lazatin <lazatin@mclinica.com>
2021-03-08 15:26:41 +0800

    Squashed 'FlutterModules/' content from commit e538791
    git-subtree-dir: FlutterModules
    git-subtree-split: e53879188b25c252efe5f4cc154a097cd8fed353

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:demo_route/screen/dashboard.dart';
+import 'package:demo_route/screen/login.dart';
+import 'package:demo_route/screen/settings.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LoginPage(),
+        '/login': (context) => LoginPage(),
+        '/dashboard': (context) => DashboardPage(),
+        '/settings': (context) => SettingsPage(),
+      },
+    );
+  }
+}
